82a575b3fb4226015b083bbe67a357159f389caa
Javier Vivas <javivasv@gmail.com>
2023-02-20 14:34:27 -0500

    Cors

--- index.ts
+++ index.ts
@@ -1,11 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const cors = require("cors")
 
 mongoose
   .connect("mongodb://localhost:27017/pndb", { useNewUrlParser: true })
   .then(() => {
     const app = express();
+    app.use(cors({
+      origin: "*",
+    }));
     app.use(express.json());
     app.use("/api", routes);
 
