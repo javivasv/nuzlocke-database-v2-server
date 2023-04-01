24b7ba220f4dc2f642e368b55f21a5aeb80a94f3
Javier Vivas <javivasv@gmail.com>
2023-04-01 12:17:46 -0400

    Token validation handle server error

--- middlewares/validateToken.ts
+++ middlewares/validateToken.ts
@@ -10,8 +10,10 @@ export function validateToken(req: Request, res: Response, next: NextFunction) {
   } catch (error) {
     if (error.name === "TokenExpiredError") {
       res.status(401).send({ error, msg: "Session expired" });
-    } else {
+    } else if (error.name === "JsonWebTokenError") {
       res.status(404).send({ error, msg: "Invalid token" });
     }
+
+    res.status(500).send({ error, msg: "Server error" });
   }
 }
