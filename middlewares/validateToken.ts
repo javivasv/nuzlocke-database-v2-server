0bc6285d8872071925dfb8f0c3bd0fff3bb1bbc6
Javier Vivas <javivasv@gmail.com>
2023-04-01 10:47:52 -0400

    Token validation reestructure

--- middlewares/validateToken.ts
+++ middlewares/validateToken.ts
@@ -2,12 +2,16 @@ import { Request, Response, NextFunction } from "express";
 import { verify } from "jsonwebtoken";
 
 export function validateToken(req: Request, res: Response, next: NextFunction) {
-    const token = req.header("Authorization");
+  const token = req.header("Authorization");
 
-    try {
-        verify(token, "pndb_v2");
-        next();
-    } catch (error) {
-        res.status(404).send({ msg: "Invalid token" });
+  try {
+    verify(token, "pndb_v2");
+    next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      res.status(401).send({ error, msg: "Session expired" });
+    } else {
+      res.status(404).send({ error, msg: "Invalid token" });
     }
+  }
 }
