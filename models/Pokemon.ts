1967f01095c8ec6428bbdf40d16dc903883f6dd1
Javier Vivas <javivasv@gmail.com>
2023-04-02 14:40:41 -0400

    Fix abilities in pokemon schema

--- models/Pokemon.ts
+++ models/Pokemon.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const schema = new mongoose.Schema({
+  originalSpecies: { type: Boolean, required: true },
   species: {
     codedSpecies: { type: String, required: true },
     formattedSpecies: { type: String, required: true },
@@ -8,14 +9,17 @@ const schema = new mongoose.Schema({
   nickname: { type: String },
   location: { type: String, required: true },
   obtained: { enum: ["caught", "gifted", "hatched", "traded", "not"], type: String, required: true },
-  original: { type: Boolean, required: true },
   sprite: { type: String },
   fainted: { type: Boolean, required: true },
   types: {
     first: { enum: ["normal", "fighting", "flying", "poison", "ground", "rock", "bug", "ghost", "steel", "fire", "water", "grass", "electric", "psychic", "ice", "dragon", "dark", "fairy"], type: String, required: true },
     second: { enum: ["", "normal", "fighting", "flying", "poison", "ground", "rock", "bug", "ghost", "steel", "fire", "water", "grass", "electric", "psychic", "ice", "dragon", "dark", "fairy"], type: String },
   },
-  ability: { type: String },
+  originalAbility: { type: Boolean, required: true },
+  ability: {
+    codedAbility: { type: String },
+    formattedAbility: { type: String },
+  },
 });
 
 export default module.exports = mongoose.model("Pokemon", schema);
