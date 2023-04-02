b3c07f931538c0bb8136f15c36a90d8d5af16a35
Javier Vivas <javivasv@gmail.com>
2023-04-02 18:07:07 -0400

    Modify team schema

--- models/Team.ts
+++ models/Team.ts
@@ -3,7 +3,13 @@ import mongoose from "mongoose";
 const schema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String },
-  pokemon: [{ type: mongoose.SchemaTypes.ObjectId }]
+  pokemon: [{
+    id: { type: mongoose.SchemaTypes.ObjectId },
+    item: { type: String },
+    moves: [
+      { type: String }
+    ],
+  }]
 });
 
 export default module.exports = mongoose.model("Team", schema);
